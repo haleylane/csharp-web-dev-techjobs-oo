03d75cc3bde1a1d38b47a007d3fc2518be062f33
Haley <haleylaneart@gmail.com>
2020-10-27 19:33:15 -0500

    * TechJobsOO.sln: * JobTests.cs: * CoreCompetency.cs:
    * TechJobTests.csproj: Use Unit Testing to Verify Parts of the Job
      Class - Test the Full Constructor

---
+++ TechJobTests/JobTests.cs
@@ -0,0 +1,36 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechJobsOO;
+
+namespace TechJobTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void EmptyTest1()
+        {
+            Assert.AreEqual(10, 10, .001);
+        }
+
+        [TestMethod]
+        public void TestSettingJobId()
+        {
+            //Job test_job = new Job("Web Dev", new Employer("Sony"), new Location("Overland Park", new PositionType("Number One", new CoreCompetency("JavaScript"));
+            Job test_job = new Job();
+            Job test_job_2 = new Job();
+            Assert.AreEqual(test_job.Id + 1, test_job_2.Id);
+        }
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            Job test_job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            Assert.AreEqual(test_job.Name, "Product tester");
+            Assert.AreEqual(test_job.EmployerName.Value, "ACME");
+            Assert.AreEqual(test_job.EmployerLocation.Value, "Desert");
+            Assert.AreEqual(test_job.JobType.Value, "Quality control");
+            Assert.AreEqual(test_job.JobCoreCompetency.Value, "Persistence");
+        }
+    }
+
+}
