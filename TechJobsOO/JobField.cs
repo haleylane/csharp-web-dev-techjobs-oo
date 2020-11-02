22b8e09778f0b3329000edaaae359fef3578f402
Haley <haleylaneart@gmail.com>
2020-11-01 19:10:41 -0600

    * Program.cs: * JobField.cs: * Location.cs: * PositionType.cs: * CoreCompetency.cs:
    * Employer.cs: Assignment works

--- TechJobsOO/JobField.cs
+++ TechJobsOO/JobField.cs
@@ -3,8 +3,70 @@ namespace TechJobsOO
 {
     public abstract class JobField
     {
+        public int Id { get; }
+        private static int nextId = 1;
+        public string Value { get; set; }
+
         public JobField()
         {
+            Id = nextId;
+            nextId++;
+        }
+
+        public JobField(string value) : this()
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is JobField field &&
+                   Id == field.Id;
+                   //Value == field.Value;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
         }
     }
+
+    public class Employer : JobField
+    {
+        public Employer(string value) : base(value)
+        {
+            Value = value;
+        }
+    }
+
+    public class Location : JobField
+    {
+        public Location(string value) : base(value)
+        {
+            Value = value;
+        }
+    }
+
+    public class CoreCompetency : JobField
+    {
+        public CoreCompetency(string value) : base(value)
+        {
+            Value = value;
+        }
+    }
+
+    public class PositionType : JobField
+    {
+        public PositionType(string value) : base(value)
+        {
+            Value = value;
+        }
+    }
+
+
 }
--- TechJobsOO/PositionType.cs
+++ TechJobsOO/PositionType.cs
@@ -1,40 +1,40 @@
 ﻿using System;
-namespace TechJobsOO
-{
-    public class PositionType
-    {
-        public int Id { get; }
-        private static int nextId = 1;
-        public string Value { get; set; }
+//namespace TechJobsOO
+//{
+    //public class PositionType : JobField
+    //{
+           // public int Id { get; }
+           // private static int nextId = 1;
+       // public string Value { get; set; }
 
-        public PositionType()
-        {
-            Id = nextId;
-            nextId++;
-        }
+        //    public PositionType()
+        //    {
+        //        Id = nextId;
+        //        nextId++;
+        //    }
 
-        public PositionType(string value) : this()
-        {
-            Value = value;
-        }
+            //public PositionType(string value) : this()
+          // {
+               //Value = value;
+            //}
 
-        public override string ToString()
-        {
-                return Value;
-            
-        }
+        //    public override string ToString()
+        //    {
+        //            return Value;
 
-        public override bool Equals(object obj)
-        {
-            return obj is PositionType type &&
-                   Id == type.Id;
-        }
+        //    }
 
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id);
-        }
+        //    public override bool Equals(object obj)
+        //    {
+        //        return obj is PositionType type &&
+        //               Id == type.Id;
+        //    }
 
-        // TODO: Add custom Equals(), GetHashCode(), and ToString() methods.
-    }
-}
+        //    public override int GetHashCode()
+        //    {
+        //        return HashCode.Combine(Id);
+        //    }
+
+        //    // TODO: Add custom Equals(), GetHashCode(), and ToString() methods.
+    //}
+//}
